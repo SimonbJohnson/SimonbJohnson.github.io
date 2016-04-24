d1a3caf4db9cde17729de4c8284e8626272f5d65
SJohnson <simonbjohnson@gmail.com>
2016-04-24 21:58:52 +0100

    New style

---
+++ js/site.js
@@ -0,0 +1,78 @@
+var colors = ['#FF5722','#795548','#039BE5','#009688','#E91E63','#9C27B0'];
+
+$.ajax({
+	url:'content/content.json',
+	dataType: 'json',
+    success: function(data) {
+    	initGrid(data);
+    }
+});
+
+
+function initGrid(data){
+
+	generateGrid(data);
+	generateButtons(data);
+
+	var $grid =  $('#grid').isotope({
+	  // options
+	  itemSelector: '.grid-item',
+	  layoutMode: 'fitRows'
+	});
+
+	$('.filter-button-group').on( 'click', 'button', function() {
+		$('.filterbutton').removeClass('highlight');
+	    var filterValue = $(this).attr('data-filter');
+	    $grid.isotope({ filter: filterValue });
+	    $(this).addClass('highlight');
+	});
+}
+
+function generateGrid(data){
+	data.forEach(function(d,i){
+		var classes = 'col-md-4 grid-item';
+		d.tags.forEach(function(tag){
+			classes += ' '+tag.replace(' ','_').toLowerCase();
+		});
+		var html = '<div id="grid'+i+'" class="'+classes+'"><div class="inner"><img id="image'+i+'" src="'+d.image+'" /><div id="overlay'+i+'" class="overlay">';
+		html+='<h3 class="grid-title">'+d.title+'</h3><p class="overlaydesc">'+d.description+'</p>';
+		html +='</div></div></div>';
+
+		$('#grid').append(html);
+
+		$('#image'+i).css({"max-width": "100%", "max-height": "auto"});
+
+		var color = Math.floor((Math.random() * (colors.length-1)));
+		console.log(color);
+		$('#overlay'+i).css({'background-color':colors[color]});
+
+		$('#overlay'+i).on('click',function(){
+			window.open(d.url, '_blank');
+		});
+
+		$('#grid'+i).on("mouseenter", function(){
+			console.log('check');						
+        	$('#overlay'+i).fadeIn(400);
+    	});
+
+    	$('#grid'+i).on("mouseleave", function(){	
+        	$('#overlay'+i).stop().fadeOut(100);
+    	});
+	});
+}
+
+function generateButtons(data){
+	var filters = [];
+	data.forEach(function(d){
+		d.tags.forEach(function(tag){
+			if(filters.indexOf(tag)==-1){
+				filters.push(tag);
+			}
+		});
+	});
+
+	filters.forEach(function(f){
+		var html = '<button class="filterbutton" data-filter=".'+f.replace(' ','_').toLowerCase()+'">'+f+'</button> ';
+		$('.filter-button-group').append(html);
+	});
+}
